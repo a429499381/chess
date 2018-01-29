9ff7468de73b52c5bb5a25cf348647b0681e1247
xutao <429499381@qq.com>
2018-01-29 17:47:35 +0800

    喵了咪， 能看懂算法，做不出算法题

--- app/app1.js
+++ app/app1.js
@@ -6,30 +6,46 @@ var shootGame = function () {
     var hitLengthArray = [] // 命中有多少中排列
     var count = 0
 
-    var hit = (function () {
-        var moveX = 0
-        for (var i = 0; i < HITLENGTH; i++) {
+    var shoot = function () {
+        for (var i = 0; i < 12 * 12; i++) {
             shootsArray[i] = []
-            for (var j = 0; j < SHOOTS; j++) {
-                shootsArray[i][j] = []
-
-
-                for (var k = 0; k < SHOOTS - HITLENGTH; k++) {
-                    moveX = k
-                    for ( var z = 0; z < HITLENGTH; z++) {
-                        shootsArray[i][j][count] = true
-
-                    }
-                }Å
-                count += 1
+            for (var k = 0; k < 12; k++) {
+                shootsArray[i][k] = 1
             }
         }
 
+        // [0][0 + 0] = 0
+        // [1][0 + 1] = 0
+        // [2][0 + 2] = 0
+        // [3][0 + 3] = 0
+        // [4][1 + 0] = 0
+        // [5][1 + 1] = 0
+        // [6][1 + 2] = 0
+        // [7][1 + 3] = 0
+        for (var j = 0; j < 8; j++) {
+            for (var k = 0; k < 8; k++) {
+                for (var i = 0; i <= 4; i++) {
+                    shootsArray[count][i + k] = 0
+                }
+                count += 1
+            }
 
+            for (var k = 0; k < 8; k++) {
+                for (var i = 0; i <= 4; i++) {
+                    shootsArray[count][i + k] = 0
 
-        console.log(shootsArray)
-        console.log(hitLengthArray)
-    })()
+                    for(var z = 1; z <= 0; z--) {
+                        shootsArray[count][z] = 1
+                    }
+                }
+                count += 1
+            }
+
+        }
+    }
+    shoot()
+
+    console.log(shootsArray)
 
 }
 
